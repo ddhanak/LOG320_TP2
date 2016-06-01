76df553566e3859887939b4d0caf676c0c2a4c69
Deep <deep.dhanak.1@etsmtl.net>
2016-06-01 19:34:05 -0400

    added 3 buttons

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,15 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("FrameDemo");
         frame.setVisible(true);
+        frame.setSize(500,500);
+
+        JButton b1 = new JButton("Charger... 1");
+        JButton b2 = new JButton("Résoudre");
+        JButton b3 = new JButton("Solution");
+        frame.add(b1);
+        frame.add(b2);
+        frame.add(b3);
+
+
     }
 }
