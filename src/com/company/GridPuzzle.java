56175215c8d2d9f6f86f73e6961c0723881b2708
waroxa <waroxa2@gmail.com>
2016-06-15 16:32:59 -0400

    Added validation and printed puzzles to console

--- src/com/company/GridPuzzle.java
+++ src/com/company/GridPuzzle.java
@@ -1,15 +1,15 @@
 package com.company;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Observable;
 
 /**
- * Created by walid on 2016-06-07.
+ * Created by Equipe on 2016-06-07.
  */
 public class GridPuzzle extends Observable {
-   // public static final int MAXIMUM = 7;
+    // public static final int MAXIMUM = 7;
     private int[][] grid;
-
 
 
     public GridPuzzle(int[][] grid) {
@@ -18,7 +18,7 @@ public class GridPuzzle extends Observable {
     }
 
     public int getCase(int i, int j) {
-     return this.grid[i][j];
+        return this.grid[i][j];
     }
 
     public void setCase(int i, int j, int value) {
@@ -30,4 +30,64 @@ public class GridPuzzle extends Observable {
     public int getLength(){
         return grid.length;
     }
+
+    public boolean isGridValid(){
+        int[][]puzzle = this.grid;
+
+        ArrayList<Integer> invalidPositions = new ArrayList<Integer>();
+        invalidPositions.add(0);
+        invalidPositions.add(1);
+        invalidPositions.add(5);
+        invalidPositions.add(6);
+        invalidPositions.add(7);
+        invalidPositions.add(8);
+        invalidPositions.add(12);
+        invalidPositions.add(13);
+        invalidPositions.add(35);
+        invalidPositions.add(36);
+        invalidPositions.add(40);
+        invalidPositions.add(41);
+        invalidPositions.add(42);
+        invalidPositions.add(43);
+        invalidPositions.add(47);
+        invalidPositions.add(48);
+
+        int numberOfBlocks = 0;
+
+        for (int i=0;i<puzzle.length;i++){
+            for (int j=0;j<puzzle.length;j++){
+
+                if(invalidPositions.contains(numberOfBlocks)){
+                    //System.out.println(puzzle[i][j]+" "+invalidPositions.get(numberOfBlocks)+" "+numberOfBlocks);
+                    if(puzzle[i][j]!= 0){
+                        return false;
+                    }
+                }
+                numberOfBlocks++;
+            }
+        }
+
+        return true;
+    }
+
+    public void printGridToConsole(){
+
+        int[][]puzzle = this.grid;
+        int count = 0;
+
+        for (int i=0;i<puzzle.length;i++){
+
+            for (int j=0;j<puzzle.length;j++){
+                count++;
+
+                if(count==7){
+                    System.out.println(puzzle[i][j]);
+                }else{
+                    System.out.print(puzzle[i][j]);
+                }
+            }
+            count=0;
+        }
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,24 +13,47 @@ public class Main {
         String gridFile = args[0];
 
         gPuzzle = new GridPuzzle(FileHelper.getPuzzleFromFile(gridFile));
-        pSolver = new PuzzleSolver(gPuzzle);
+        //boolean isValid = gPuzzle.isGridValid();
 
-        long startTime = System.currentTimeMillis();
-        if (pSolver.solvePuzzle()){
-            long endTime = System.currentTimeMillis();
-            System.out.println("Temps d'execution: " + (endTime - startTime) + " milisecondes");
-            System.out.println("Solution : "+"Résolu");
-            System.out.println("Nombre de noeuds visités : " + pSolver.getNbPositionsVisited());
-            System.out.println("Nombre de coups de la solution : " + pSolver.getMoves().size());
-        }else{
-            System.out.println("Solution : "+"Pas de Solution");
-            System.out.println("Nombre de noeuds visités : " + pSolver.getNbPositionsVisited());
-            System.out.println("Nombre de coups de la solution : " + pSolver.getMoves().size());
-        }
+        //if (isValid){
+            pSolver = new PuzzleSolver(gPuzzle);
+            System.out.println("Puzzle à jouer:");
+            System.out.println("=================================");
+            gPuzzle.printGridToConsole();
+            System.out.println("=================================");
+            long endTime;
 
-        for (Move move : pSolver.getMoves()) {
-            System.out.println(move.start + " to " + move.end);
-        }
+            long startTime = System.currentTimeMillis();
+            if (pSolver.solvePuzzle()){
+                endTime = System.currentTimeMillis();
+                System.out.println("Temps d'execution: " + (endTime - startTime) + " milisecondes");
+                System.out.println("Solution : "+"Résolu");
+                System.out.println("Nombre de noeuds visités : " + pSolver.getNbPositionsVisited());
+                System.out.println("");
+                System.out.println("Puzzle résolu:");
+                System.out.println("=================================");
+                gPuzzle.printGridToConsole();
+                System.out.println("=================================");
+                System.out.println("Nombre de coups de la solution : " + pSolver.getMoves().size());
+            }else{
+                endTime = System.currentTimeMillis();
+                System.out.println("Temps d'execution: " + (endTime - startTime) + " milisecondes");
+                System.out.println("Solution : "+"Pas de Solution");
+                System.out.println("Nombre de noeuds visités : " + pSolver.getNbPositionsVisited());
+                System.out.println("Nombre de coups de la solution : " + pSolver.getMoves().size());
+            }
+
+            for (Move move : pSolver.getMoves()) {
+                System.out.println(move.start + " to " + move.end);
+            }
+
+            System.out.println("=================================");
+
+        //}else{
+
+        //    System.out.println("Invalid Game. Cannot be played.");
+        //    System.out.println("Please enter a valid game");
+        //}
 
     }
 }
